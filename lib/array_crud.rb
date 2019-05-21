73f3287cdbbec652e72daa9626c3ad400c44e489
Aida Abdikulova <abdikulova@gmail.com>
2019-05-21 03:14:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["dog", "cat", "fish", "horse"]
+  ["horse", "dog", "cat", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+  array(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  array[-1]
+
 end
